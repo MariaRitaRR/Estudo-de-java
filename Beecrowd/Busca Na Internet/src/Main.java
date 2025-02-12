82fcb606caefb01146108b62c90369e00725cf87
MariaRitaRR <mariaritaraposo@outlook.com>
2025-02-12 17:49:05 -0300

    Site

---
+++ Beecrowd/Busca Na Internet/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int t = entrada.nextInt();
+
+        System.out.println((t*4));
+
+        entrada.close();
+    }
+}
